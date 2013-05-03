39a612709874760b52d6af3f9488aabea50c7dff
David Goehrig <dave@dloh.org>
2013-05-03 04:09:19 +0000

    Adding README w/ license

--- bohemian.erl
+++ bohemian.erl
@@ -1,3 +1,10 @@
+%% bohemian.erl
+%% 
+%% © 2013 David J Goehrig <dave@dloh.org>
+%% All Rights Reserved
+%%
+%%	Bohemian for Erlang function objects with private state.
+%%
 
 -module(bohemian).
 -export([ object/1,state/0 ]).
@@ -13,9 +20,10 @@ state() ->
 object(Parents) ->
 	State = spawn_link(?MODULE,state,[]),
 	May = fun(Self, Method) ->
+		io:format("Looking up ~p with parents ~p~n", [ Method, Parents ]),
 		case lists:filter(fun(X) -> is_function(X) end, 
-			lists:map(fun(P) -> P([lookup, Method]) end, Self([parents]))) of
-			[F] when is_function(F) -> F;
+			lists:map(fun(P) -> P([lookup, Method]) end, Parents)) of
+			[F|X] when is_function(F) -> F;
 			_ -> false
 		end
 	end,
@@ -32,13 +40,19 @@ object(Parents) ->
 	Has = fun(_Self, Property, Value) ->	
 		State ! { self(), Property, fun(_Self) -> Value end }
 	end,
+	Unknown = fun(Self,Method,Args) ->
+		io:format("~p doesn't ~p with ~p~n", [ Self, Method, Args]) 
+	end,
 	Y = fun([M|A]) -> 
 		G = fun(Self,Method,Args) ->
 			case Lookup(Self,Method) of
 				F when is_function(F) -> apply(F, [ fun([MM|AA]) -> Self(Self,MM,AA) end | Args ]);
 				_  -> case May(Self,Method) of
 					FF when is_function(FF) -> apply(FF, [ fun([MM|AA]) -> Self(Self,MM,AA) end | Args ]);
-					_ -> Self(['*',Method,Args])
+					_ -> apply(Lookup(Self,'*'), [ 	
+						fun([MMM|AAA]) -> 
+							Self(Self,MMM,AAA) end, 
+						Method , Args ])
 				end
 			end
 		end,
@@ -47,9 +61,5 @@ object(Parents) ->
 	State ! { self(), does, Does },
 	State ! { self(), has, Has },
 	State ! { self(), lookup, Lookup },
-	State ! { self(), parents, fun(Self) -> Parents end },
-	State ! { self(), is, fun(Self,Parent) -> 
-			Self([has, parents, [Parent | Self([parents])]) end }.
-	State ! { self(), '*', fun(Self,Method,Args) ->
-		io:format("~p doesn't ~p with ~p~n", [ Self, Method, Args ) end }.
+	State ! { self(), '*', Unknown },
 	Y.
